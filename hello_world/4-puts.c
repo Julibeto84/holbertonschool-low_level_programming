a0fdcc1927728bc4ad7791b5cf195a1810c58f3b
Julibeto84 <juliangp670@gmail.com>
2023-02-20 12:28:11 -0800

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,12 @@
 /**
- *  * main - Prints "\"Programming is like building a multilingual puzzle"
- *   * Return: 0
- *    */
+ * main - Prints "\"Programming is like building a multilingual puzzle"
+ * Return: 0
+ */
 
 #include <stdio.h>
 
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
 }
