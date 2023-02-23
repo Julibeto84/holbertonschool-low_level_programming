09f0762196b673d5f764daa8f60a10333804a898
Julibeto84 <juliangp670@gmail.com>
2023-02-23 09:14:13 -0800

    6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* _abs - Returnts absolute value
+* @c: to be evaluated
+* Return: Absolute value of n
+*/
+int _abs(int n)
+{
+if (n > 0)
+return (n);
+else
+return (n * -1);
+}
