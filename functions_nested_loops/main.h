ee4acad57bbc5b2c6041f2206cae3f79ec3df80e
Julibeto84 <juliangp670@gmail.com>
2023-02-22 16:46:16 -0800

    prototipo_0

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,12 +1,1 @@
-int main(void)
-{
-  _putchar('_');
-  _putchar('p');
-  _putchar('u');
-  _putchar('t');
-  _putchar('c');
-  _putchar('h');
-  _putchar('a');
-  _putchar('r');
-  _putchar('\n');
-  return (0);
+putchar.c 0-putchar.c -o 0-putchar
