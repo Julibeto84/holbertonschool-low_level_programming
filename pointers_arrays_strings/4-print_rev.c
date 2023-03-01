0fb6700011e4714f0150f3f6dd879e223b39ac0b
Julibeto84 <juliangp670@gmail.com>
2023-03-01 08:48:37 -0800

    4-print_rev.c

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,16 +1,16 @@
 #include "main.h"
 #include <string.h>
 /**
-* _strlen - returns the length of a string.
-* @s: Pointer to string to measure
-* Return: Lenght of the string
+ * _strlen - returns the length of a string.
+ * @s: Pointer to string to measure
+ * Return: Lenght of the string
 */
 void print_rev(char *s)
 {
 int len = strlen(s);
 for (int i = len - 1; i >= 0; i--)
 {
-_putchar("%c", s[i]);
+ _putchar("%c", s[i]);
 }
-_putchar("\n");
+ _putchar("\n");
 }
