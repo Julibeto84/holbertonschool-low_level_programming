3a0a690ca591cccb2ae6f10e63c439f16eabe2d7
Julibeto84 <juliangp670@gmail.com>
2023-03-01 08:58:15 -0800

    4-print_rev.c

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -7,10 +7,10 @@
 */
 void print_rev(char *s)
 {
-int len = strlen(s);
-for (int i = len - 1; i >= 0; i--)
+int len = strlen(s) - 1;
+for (; len >= 0; len = len - 1)
 {
- _putchar("%c", s[i]);
+  _putchar(*(s + len));
 }
  _putchar("\n");
 }
