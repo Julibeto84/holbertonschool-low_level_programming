e07a9efc54e2ffa5d608eb21bc7a3e9111e562f5
Julibeto84 <juliangp670@gmail.com>
2023-03-01 09:24:41 -0800

    4-print_rev.c

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,16 +1,21 @@
 #include "main.h"
-#include <string.h>
 /**
- * _strlen - returns the length of a string.
- * @s: Pointer to string to measure
- * Return: Lenght of the string
+* print_rev - print reverse characters.
+* @s: Pointer to string to reverse
+* Return: Always 0.
 */
 void print_rev(char *s)
 {
-int len = strlen(s) - 1;
-for (; len >= 0; len = len - 1)
+int i = 0;
+while (s[i] != '\0')
 {
-  _putchar(*(s + len));
+i++;
 }
- _putchar("\n");
+i--;
+for (; i >= 0; i--)
+{
+_putchar(s[i]);
 }
+_putchar('\n');
+}
+
