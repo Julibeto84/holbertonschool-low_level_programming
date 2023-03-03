23f4664ddd5c26ff3e4eaa5b8dadd2958f772275
Julibeto84 <juliangp670@gmail.com>
2023-03-03 06:08:13 -0800

    5-string_toupper.c

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * string_toupper - "(char *)" 
+ * string_toupper - "(char *)"
  * @s: String to change
  * Return: Pointer to string
- */
+*/
 
 
 char *string_toupper(char *str)
@@ -15,7 +15,7 @@ char *string_toupper(char *str)
 		if (*ptr >= 'a' && *ptr <= 'z')
 		{
 			*ptr = *ptr - ('a' - 'A');
-		}		
+		}
 		ptr++;
 	}
 	return str;
