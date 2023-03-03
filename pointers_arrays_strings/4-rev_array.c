1a22b1b9311276aa19b1b0fb7477024acaf7636a
Julibeto84 <juliangp670@gmail.com>
2023-03-02 19:17:38 -0800

    4-rev_array.c

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -6,11 +6,13 @@
  * @n: number of elements of the array
  * Return: Always 0
  */
-void reverse_array(int *a, int n) {
+void reverse_array(int *a, int n)
+{
 	int temp;
 	int i, j;
 
-	for (i = 0, j = n - 1; i < j; i++, j--) {
+	for (i = 0, j = n - 1; i < j; i++, j--)
+	{
 		temp = a[i];
 		a[i] = a[j];
 		a[j] = temp;
