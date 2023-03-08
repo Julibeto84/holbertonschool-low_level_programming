6b86659d16d2966bd1f7828d2a6dd60901ee5f5d
Julibeto84 <juliangp670@gmail.com>
2023-03-08 07:32:23 -0800

    4-pow_recursion.c

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _pow_recursion - Calculates the power of an int "x" raised to another int "y"
+ * @x: base.
+ * @y: exponent.
+ * Return: x raised to y.
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1); /** if (y < 0) : Verifica si y es menor que cero. Si es así, la función devuelve -1*/
+	else if (y == 0) /**else if (y == 0) : Verifica si y es igual a cero. Si es así, la función devuelve 1*/
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
