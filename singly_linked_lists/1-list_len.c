cd8077cc6b2f19577b06dd2d021fb6bb3cf16029
Julibeto84 <juliangp670@gmail.com>
2023-04-04 20:25:50 -0700

    1-list_len.c

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,26 +1,18 @@
 #include "lists.h"
 
 /**
- * print_list - prints all the elements of a list_t list.
+ * list_len - Number .
  * @h: pointer to list to print
- * Return: Number of nodes
+ * Return: Number of members
  */
 
-size_t print_list(const list_t *h)
+size_t list_len(const list_t *h)
 {
 	size_t ret;
 
 	ret = 0;
 	while (h != NULL)
 	{
-		if (h->str == NULL)
-		{
-			printf("[0] (nil)\n");
-			h = h->next;
-			ret += 1;
-			continue;
-		}
-		printf("[%i] %s\n", h->len, h->str);
 		ret += 1;
 		h = h->next;
 	}
